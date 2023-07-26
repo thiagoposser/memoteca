b7c7e0a35994669a426d077b4d6fd10563d2630e
Thiago Posser da Silva <thiagoposser@yahoo.com.br>
2023-07-26 16:44:07 -0300

    lista favorito

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -41,4 +41,13 @@ export class ListarPensamentoComponent implements OnInit {
     })
 
   }
+
+  listarFavoritos(){
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service.listarPensamentosFavoritos(this.paginaAtual, this.filtro)
+      .subscribe(listarPensamentosFavoritos => {
+        this.listaPensamentos = listarPensamentosFavoritos
+      })
+  }
 }
